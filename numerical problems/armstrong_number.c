e0d93db55928a8f7ab59bc638814b34a49a2c1a5
surprisedPikachu69 <arvind7447@gmail.com>
2022-09-16 12:39:25 +0530

    armstrong number - dis is da wae

--- numerical problems/armstrong_number.c
+++ numerical problems/armstrong_number.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int n = 153;
+    int n = 1;
     int original = n;
     int sum = 0, count = 0;
 
@@ -13,8 +13,8 @@ int main() {
     n = original;
     while(n != 0) {
         int num = n%10;
-        int partial_sum = num;
-        for(int i = 0; i < count-1; i++)
+        int partial_sum = 1;
+        for(int i = 0; i < count; i++)
             partial_sum *= num;
 
         sum += partial_sum;
