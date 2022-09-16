d61893177d63a7eb0fbe842948692ea6105ecf30
surprisedPikachu69 <arvind7447@gmail.com>
2022-09-16 11:06:09 +0530

    prime with improved efficiency

--- numerical problems/prime.c
+++ numerical problems/prime.c
@@ -3,7 +3,7 @@
 int main() {
     int n = 23, flag = 1;
 
-    for(int i = 2; i <= n/2; i++) {
+    for(int i = 2; i*i <= n/2; i++) { // i*i <=n decreases execution time exponentially for worst case
         if(n%i == 0) { 
             flag = 0;
             break;
