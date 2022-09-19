d9ef7ac013bb722a212553a66f2697f89afd8af8
surprisedPikachu69 <arvind7447@gmail.com>
2022-09-19 15:22:23 +0530

    unique elements - without duplicates

--- arrays/unique_elements.c
+++ arrays/unique_elements.c
@@ -13,4 +13,15 @@ int main() {
         }
         if(j == n) printf(" %d",arr[i]);
     }
+
+    // eliminating duplicates
+    printf("\nwithout duplicates: ");
+    for(int i = 0; i < n; i++) {
+        int j;
+        for(j = 0; j < i; j++) {
+            if(i == j) continue;
+            if(arr[i] == arr[j]) break;
+        }
+        if(j == i) printf(" %d",arr[i]);
+    }
 }
