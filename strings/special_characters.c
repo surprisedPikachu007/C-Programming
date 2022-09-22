e0b3d1892d61fc9073d24bcb0e2c352187142b5b
surprisedPikachu69 <arvind7447@gmail.com>
2022-09-22 15:57:16 +0530

    special characters - revised

--- strings/special_characters.c
+++ strings/special_characters.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-#define isspecial(i) (ch == 9) || (ch == 10) || (ch == 13) || (ch[i] >= 32 && ch[i] <= 47) || (ch[i] >= 58 && ch[i] <= 64) || (ch[i] >= 91 && ch[i] <= 96)  || (ch[i] >= 123 && ch[i] <= 126)
-
 int main() {
     char ch[] = "$(dollar)! = (rupees/75)";
     int count = 0;
 
     for(int i = 0; ch[i] != '\0'; i++) 
-        if(isspecial(i))
+        if((ch[i] >= 'a' && ch[i] <= 'z') || (ch[i] >= 'A' && ch[i] <= 'Z') || (ch[i] >= '0' && ch[i] <= '9'))
+            continue;
+        else
             count++;
 
     printf("special character count: %d\n",count);
