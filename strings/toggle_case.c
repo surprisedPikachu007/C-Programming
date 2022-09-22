622b77985af3ad8c61e6d2d57a218be9967c8989
surprisedPikachu69 <arvind7447@gmail.com>
2022-09-22 19:24:55 +0530

    toggle case with explanation

--- strings/toggle_case.c
+++ strings/toggle_case.c
@@ -3,6 +3,12 @@
 int main() {
     char ch[] = "hELlo";
 
+    /* Here is the explanation for the code above:
+    1. We create a loop that will run from 0 to the length of the string.
+    2. If the character at index i is a lowercase letter, we subtract 32 to convert it to uppercase.
+    3. If the character at index i is an uppercase letter, we add 32 to convert it to lowercase.
+    4. We display the changed string. */
+
     for(int i = 0; ch[i] != '\0'; i++) {
         if(ch[i] >= 'a' && ch[i] <= 'z')
             ch[i] -= 32;
