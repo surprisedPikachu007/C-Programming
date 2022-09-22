766472777fd2ec70ac953d6713b21bc2548fb54d
surprisedPikachu69 <arvind7447@gmail.com>
2022-09-22 15:30:43 +0530

    special characters

---
+++ strings/special_characters.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define isspecial(i) (ch[i] >= 32 && ch[i] <= 47) || (ch[i] >= 58 && ch[i] <= 64) || (ch[i] >= 91 && ch[i] <= 96)  || (ch[i] >= 123 && ch[i] <= 126)
+
+int main() {
+    char ch[] = "$(dollar)! = (rupees/75)";
+    int count = 0;
+
+    for(int i = 0; ch[i] != '\0'; i++) 
+        if(isspecial(i))
+            count++;
+
+    printf("special character count: %d\n",count);
+}
