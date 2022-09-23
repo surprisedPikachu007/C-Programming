c0208d5b7e50fadb69edea70e35a83eaa38aeb09
surprisedPikachu69 <arvind7447@gmail.com>
2022-09-23 14:23:39 +0530

    frequency of characters in string's order

--- strings/frequency_of_characters.c
+++ strings/frequency_of_characters.c
@@ -4,16 +4,17 @@ int main() {
     char ch[] = "Drag0nsLayer@cod.com";
     int frequencies[128] = {};
 
-    printf("given string: %s", ch);
+    printf("given string: %s\n", ch);
 
     for(int i = 0; ch[i] != '\0'; i++) {
         frequencies[ch[i]]++;
     }
 
     printf("frequrncy of characters: \n");
-    for(int i = 0; i < 128; i++) {
-        if(frequencies[i]) {
-            printf("%c: %d\n", i, frequencies[i]);
+    for(int i = 0; ch[i] != '\0'; i++) {
+        if(frequencies[ch[i]]) {
+            printf("%c: %d\n", ch[i], frequencies[ch[i]]);
+            frequencies[ch[i]] = 0;
         }
     }
 }
